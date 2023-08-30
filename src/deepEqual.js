c3cd011a190e208282fa6f44de57a6f3258151e7
Alan George <55777241+Alan332@users.noreply.github.com>
2023-08-30 01:34:59 +0000

    Initial commit

--- src/deepEqual.js
+++ src/deepEqual.js
@@ -1,30 +1,4 @@
 const deepEqual = (a, b) => {
-  if (a === b) {
-    return true;
-  }
-  else if (typeof(a) == 'object' && typeof(b) == 'object'){
-    if(JSON.stringify(a) === JSON.stringify(b)){
-    return true;
-    }
-    else {
-      const keys1 = Object.keys(a);
-      const keys2 = Object.keys(b);
-      if (keys1.length !== keys2.length) {
-        for (let key of keys1) {
-          if ((a[key] !== b[key]) && a[key] !== 'undefind' && b[key] !== 'undefind') {
-            return false;
-          }
-          else {
-            return true;
-          }
-        }
-      }
-    }
-  }
-  else {
-    return false;
-  }
-  
+  return true
 }
-
 module.exports = deepEqual;
