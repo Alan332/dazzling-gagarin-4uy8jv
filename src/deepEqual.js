00715e9820e1e4132e7c1ea20222c398fb6a8b4c
CodeSandbox <support@codesandbox.io>
2023-08-30 01:29:01 +0000

    Sandbox snapshot commit

--- src/deepEqual.js
+++ src/deepEqual.js
@@ -1,5 +1,30 @@
 const deepEqual = (a, b) => {
-  return false;
+  if (a === b) {
+    return true;
+  }
+  else if (typeof(a) == 'object' && typeof(b) == 'object'){
+    if(JSON.stringify(a) === JSON.stringify(b)){
+    return true;
+    }
+    else {
+      const keys1 = Object.keys(a);
+      const keys2 = Object.keys(b);
+      if (keys1.length !== keys2.length) {
+        for (let key of keys1) {
+          if ((a[key] !== b[key]) && a[key] !== 'undefind' && b[key] !== 'undefind') {
+            return false;
+          }
+          else {
+            return true;
+          }
+        }
+      }
+    }
+  }
+  else {
+    return false;
+  }
+  
 }
 
 module.exports = deepEqual;
